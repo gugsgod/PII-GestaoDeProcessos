f703658b6494c0f67307a3554e7ad36624e16a49
Caio Onha Ferreira <caioonha@gmail.com>
2025-10-02 10:51:49 -0300

    Adicionando pasta de assets

--- src/lib/src/pages/PaginaInicial.dart
+++ src/lib/src/pages/PaginaInicial.dart
@@ -5,9 +5,14 @@ class PaginaInicial extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
+            debugShowCheckedModeBanner: false,
             home: Scaffold(
                 appBar: AppBar(
-
+                    backgroundColor: Colors.white,
+                    leading: Padding(
+                      padding: const EdgeInserts.all(8.0),
+                      child: Image.asset('assets/Sao_Paulo_Metro_Logo.svg'),
+                    ),
                 ),
             )
         );
